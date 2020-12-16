81f82a26f536b3a8c8457febc7640d2eb787be2e
Ken Charette <charettekenneth@gmail.com>
2020-12-15 20:26:24 -0500

    Set up a test db and post route for submitting the form

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)  # __name__ is just for referencing this file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)  # initializing the database
 
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])  # syntax to add in the methods
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']  # task_content variable is the contents of the form
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.data_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
 if __name__ == "__main__":
